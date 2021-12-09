1adc52bb240126b995601fe5633f3b338ead3e43
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-08 22:00:14 -0600

    add html and js methods to practice getting started with vue

--- index.js
+++ index.js
@@ -6,3 +6,45 @@ var app = new Vue({
 });
 
 app.message = 'I have changed the message';
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    //app.message = 'type message here' will change message in console
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    //app.seen = false in console will do the magic
+    seen: true,
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      //app4.todos.push({ text: 'New Item' }) in console
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+
