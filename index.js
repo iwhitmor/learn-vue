88051dc848b0966e3af39335740dc18a0417336c
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-08 22:12:25 -0600

    create vue component and use it to render new todo items on page

--- index.js
+++ index.js
@@ -58,4 +58,20 @@ var app6 = new Vue({
 })
 
 
+//Creating a component with pre-defined options that accepts a prop
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue ({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Poop' }
+    ]
+  }
+})
 
